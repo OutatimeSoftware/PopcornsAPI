ce94d1d1206b3525dadc584da3f3f8ba6386f90a
Mario J. Chan Zurita <56327994+MarioJChanZurita@users.noreply.github.com>
2021-01-25 13:36:39 -0600

    api

---
+++ PopcornsAPI/src/main/java/com/popcorn/PopcornsAPI/PopcornsApiApplication.java
@@ -0,0 +1,13 @@
+package com.popcorn.PopcornsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornsApiApplication.class, args);
+	}
+
+}
---
+++ PopcornsAPI/src/test/java/com/popcorn/PopcornsAPI/PopcornsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popcorn.PopcornsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
