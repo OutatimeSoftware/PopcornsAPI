28517b60f3dbee46443e62d4acb7d190bf8a3d6a
Jorge Reyes <reyestrevjorge@gmail.com>
2021-01-28 23:27:05 -0600

    feat: Created index.html

--- src/main/java/com/popcorn/PopcornsAPI/PopcornsApiApplication.java
+++ src/main/java/com/popcorn/PopcornsAPI/PopcornsApiApplication.java
@@ -2,15 +2,28 @@ package com.popcorn.PopcornsAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Controller
 public class PopcornsApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PopcornsApiApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String index(
+			Model model
+	) {
+
+		model.addAttribute("name", "Jorge");
+
+		return "index";
+	}
 
 
 }
